2ba4b132b5650a0ee8ade0e6457e0af5c6e9d26d
Jan Kapała <jankapala@gmail.com>
2019-08-29 08:45:01 +0200

    Added __repr__ method for the Winnings class

--- src/extensions/games/tic_tac_toe/tic_tac_toe_logic.py
+++ src/extensions/games/tic_tac_toe/tic_tac_toe_logic.py
@@ -59,6 +59,8 @@ class Winning:
     def __str__(self):
         return str(self.mark) + ", start: " + str(self.starting_point) + ", end: " + str(self.ending_point)
 
+    def __repr__(self):
+        return self.__str__()
 
 class Board:
     """Class representing a Tic Tac Toe board.
