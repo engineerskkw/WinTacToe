e00c7bb5042b9014e5fc6c5e898377d0ad15da78
Jan Kapała <jankapala@gmail.com>
2019-08-29 08:43:37 +0200

    Bugfixes in OpenAI gym tic_tac_toe_basic_env and added functions: get_current_state, random_initial_state, _regenerate_possible_actions

--- src/extensions/games/tic_tac_toe/tic_tac_toe/envs/tic_tac_toe_basic_env.py
+++ src/extensions/games/tic_tac_toe/tic_tac_toe/envs/tic_tac_toe_basic_env.py
@@ -1,6 +1,7 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import random
 
 
 # Workaround import
@@ -8,10 +9,14 @@ import sys
 sys.path.insert(1, '../../..')
 from tic_tac_toe_logic import *
 
-INVALID_ACTION_REWARD = -10
 EVERY_ACTION_REWARD = -1
 WIN_REWARD = 10
 LOOSE_REWARD = -10
+TIE_REWARD = 0
+
+NOT_END = 0
+END_BY_WIN = 1
+END_BY_DRAW = 2
 
 class TicTacToeBasicEnv(gym.Env):
 	metadata = {'render.modes':['human']}
@@ -26,30 +31,45 @@ class TicTacToeBasicEnv(gym.Env):
 		self.observation_space = spaces.MultiDiscrete([size, size])
 		self.action_space = spaces.MultiDiscrete([size, size])
 
+		self.possible_actions = []
+		self._regenerate_possible_actions()
+
+		self.next_step_done = NOT_END
+
 	def step(self, action, player):
+		# End game case
+		if self.next_step_done:
+			state = self.env.board.board
+			if self.next_step_done == END_BY_WIN:
+				reward = LOOSE_REWARD
+			if self.next_step_done == END_BY_DRAW:
+				reward = TIE_REWARD
+			return state, reward, True, {"metadata"}
+
+		# Move
 		x,y = action
-
-		reward = 0
-		reward += EVERY_ACTION_REWARD
-
 		try:
-			if not self.env.place_mark(x, y):
-				reward += INVALID_ACTION_REWARD
+			self.env.place_mark(x, y)
+			state = self.env.board.board
 		except IndexError as error:
 			print("These are not valid coordinates...\n")
+		self._regenerate_possible_actions()
 
-		state = self.env.board.board
-		done = False
-
+		
 		self.current_winnings = self.env.gather_winnings()
+		# Win/loose case
 		if self.current_winnings:
-			done = True
-			if player in self.current_winnings:
-				reward += WIN_REWARD
-			else:
-				reward += LOOSE_REWARD
+			self.next_step_done = END_BY_WIN
+			reward = WIN_REWARD
+		# Draw/no more moves case
+		elif self.possible_actions == []:
+			self.next_step_done = END_BY_DRAW
+			reward = TIE_REWARD
+		else:
+			reward = EVERY_ACTION_REWARD
 
-		return state, reward, done, {"metadata"}
+		
+		return state, reward, False, {"metadata"}
 
 	def reset(self):
 		self.env = TicTacToeLogic(players, size, marks_required)
@@ -57,6 +77,63 @@ class TicTacToeBasicEnv(gym.Env):
 	def render(self, mode='human'):
 		if mode == 'human':
 			print(self.env.board.board)
+
+	def get_current_state(self):
+		return self.env.board.board
+
+	def random_initial_state(self):
+		### Uniform random initialization, but without
+		# Game-endind or illegal(win of both players) states
+
+		# All possible players' marks and empty mark
+		players_marks = [-1]
+		for p in self.env.players:
+			players_marks.append(p.mark)
+
+		# Random fields filling order
+		coords = []
+		size = self.env.board.size
+		for v in range(size):
+			for h in range(size):
+				coords.append((v,h))
+
+		random.shuffle(coords)
+		n = random.randint(0,len(coords)) # Number of fields to fill
+
+		# Random fields value
+		for i in range(n):
+			v,h = coords[i]
+			random_mark = random.choice(players_marks)
+			self.env.board.place_mark(v,h,random_mark)
+			if self.env.gather_winnings():
+				self.env.board.board[v,h] = -1
+
+		# Check all fields filled
+		# if so, then randomly unmark one of them
+		all_filled = True
+		for v in range(size):
+			for h in range(size):
+				if self.env.board.board[v,h] == -1:
+					all_filled = False
+					break
+
+		if all_filled:
+			v = random.randint(0,size-1)
+			h = random.randint(0,size-1)
+			self.env.board.board[v,h] = -1
+
+		self._regenerate_possible_actions()
+
+		return self.env.board.board
+
+	def _regenerate_possible_actions(self):
+		self.possible_actions = []
+		board = self.env.board.board
+		height, width = board.shape
+		for h in range(height):
+			for v in range(width):
+				if board[h,v] == -1:
+					self.possible_actions.append([h, v])
 
 	# def close(self):
 	# 	pass
