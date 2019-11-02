d9aeee5d0131398dfa2f7897af375fc8db7534e3
jankapala <jankapala@gmail.com>
2019-11-02 22:01:49 +0100

    Make project root path absolute

--- game_app/games/tic_tac_toe/tic_tac_toe_component.py
+++ game_app/games/tic_tac_toe/tic_tac_toe_component.py
@@ -1,7 +1,9 @@
 #BEGIN--------------------PROJECT-ROOT-PATH-APPENDING-------------------------#
 import sys, os
-PROJECT_ROOT_PATH = "./../../../"
-sys.path.append(os.path.join(os.path.dirname(__file__), PROJECT_ROOT_PATH))
+REL_PROJECT_ROOT_PATH = "./../../../"
+ABS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+ABS_PROJECT_ROOT_PATH = os.path.normpath(os.path.join(ABS_FILE_DIR, REL_PROJECT_ROOT_PATH))
+sys.path.append(ABS_PROJECT_ROOT_PATH)
 #-------------------------PROJECT-ROOT-PATH-APPENDING----------------------END#
 
 from thespian.actors import *
