116bcbf34cca07302bbb420a7a0a980afcb6646b
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-11-12 00:10:09 +0100

    Add basic update_rule and take action methods

--- reinforcement_learning/agents/q_learning_agent/q_learning_agent.py
+++ reinforcement_learning/agents/q_learning_agent/q_learning_agent.py
@@ -13,17 +13,33 @@ sys.path.append(ABS_PROJECT_ROOT_PATH)
 from
 
 class QLearningAgent(Agent):
-    def __init__(self, step_size, epsilon):
+    def __init__(self, step_size, epsilon, delta):
         self.action_value = LazyTabularActionValue()
         self.policy = ActionValueDerivedPolicy(self.action_value)
         self.step_size = step_size
         self.epsilon = epsilon
+        self.delta = delta
+
+        self.prev_action = None
+        self.prev_state = None
+        self.prev_reward = None
 
     def take_action(self, state, allowed_actions):
-        pass
+        self.update_rule(self.prev_state, self.prev_action, self.prev_reward, state)
+        action = self.policy.epsilon_greedy(state, allowed_actions, self.epsilon)
+        self.prev_action = action
+        self.prev_state = state
+
+        return action
 
     def receive_reward(self, reward):
-        pass
+        self.prev_reward = reward
+
+    def update_rule(self, prev_state, prev_action, reward_for_prev_action, current_state):
+        prev_action_value = self.action_value[prev_state, prev_action]
+        error = self.step_size * \
+                (reward_for_prev_action + self.delta * self.action_value.max_over_actions(current_state) - prev_action_value)
+        self.action_value[prev_state, prev_action] = prev_action_value + error
 
     def exit(self, terminal_state):
         pass
