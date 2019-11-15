feb2cbc73c97d2bad9e2c35dd05563d2cec049c2
Jan K. Kapala <jankapala@gmail.com>
2019-11-15 12:06:30 +0100

    Add start_monitor_client script to examples

---
+++ training_platform/examples/start_monitor_client.py
@@ -0,0 +1,15 @@
+# BEGIN--------------------PROJECT-ROOT-PATH-APPENDING-------------------------#
+import sys
+import os
+REL_PROJECT_ROOT_PATH = "./../../"
+ABS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+ABS_PROJECT_ROOT_PATH = os.path.normpath(os.path.join(ABS_FILE_DIR, REL_PROJECT_ROOT_PATH))
+sys.path.append(ABS_PROJECT_ROOT_PATH)
+# -------------------------PROJECT-ROOT-PATH-APPENDING----------------------END#
+
+from training_platform import MonitorClient
+
+if __name__ == '__main__':
+    monitor = MonitorClient()
+    print("Monitor Client has started!")
+    monitor.start_monitoring()
