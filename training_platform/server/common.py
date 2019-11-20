1db96e73cad7dfb7db33baff42cf12d26690df1b
Jan K. Kapala <jankapala@gmail.com>
2019-11-20 17:56:41 +0100

    Refactor code in preambulas

--- training_platform/server/common.py
+++ training_platform/server/common.py
@@ -1,17 +1,19 @@
-#BEGIN--------------------PROJECT-ROOT-PATH-APPENDING-------------------------#
-import sys, os
+# BEGIN--------------------PROJECT-ROOT-PATH-APPENDING-------------------------#
+import sys
+import os
 REL_PROJECT_ROOT_PATH = "./../../"
 ABS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 ABS_PROJECT_ROOT_PATH = os.path.normpath(os.path.join(ABS_FILE_DIR, REL_PROJECT_ROOT_PATH))
 sys.path.append(ABS_PROJECT_ROOT_PATH)
-#-------------------------PROJECT-ROOT-PATH-APPENDING----------------------END#
+# -------------------------PROJECT-ROOT-PATH-APPENDING----------------------END#
 
+import datetime
 import configparser
 config = configparser.ConfigParser()
 config.read(os.path.join(ABS_PROJECT_ROOT_PATH, "training_platform", "config.ini"))
 ACTOR_SYSTEM_BASE = config["TRAINING PLATFORM PARAMETERS"]["actorsystembase"]
 
-import datetime
+
 
 # start_server script <-> GameManager Actor communication
 class InitGameManagerMsg:
