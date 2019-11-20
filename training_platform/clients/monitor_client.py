66848cce3b6de824e59e77f7bfc4991c1f4bdc2e
Jan K. Kapala <jankapala@gmail.com>
2019-11-20 17:49:28 +0100

    Adjust actor system base to config-based implementation and add monitor join acknowledgement

--- training_platform/clients/monitor_client.py
+++ training_platform/clients/monitor_client.py
@@ -7,27 +7,24 @@ sys.path.append(ABS_PROJECT_ROOT_PATH)
 #-------------------------PROJECT-ROOT-PATH-APPENDING----------------------END#
 
 from thespian.actors import *
-
 from training_platform.server.common import *
 from training_platform.server.service import GameManager, MatchMaker
 from training_platform.server.logger import Logger
 
+
 class MonitorClient:
     def __init__(self):
-        self.asys = ActorSystem('multiprocTCPBase')
+        self.asys = ActorSystem(ACTOR_SYSTEM_BASE)
         self.match_maker_addr = self.asys.createActor(MatchMaker, globalName="MatchMaker")
         self.game_manager_addr = self.asys.createActor(GameManager, globalName="GameManager")
         self.logger_addr = self.asys.createActor(Logger, globalName="Logger")
 
     def start_monitoring(self):
-        self.asys.tell(self.logger_addr, JoinMonitorMsg())
+        response = self.asys.ask(self.logger_addr, JoinMonitorMsg())
+        if isinstance(response, MonitorJoinAcknowledgement):
+            print("Succesfully joined logger")
 
         while True:
             msg = self.asys.listen()
             if isinstance(msg, LogMsg):
                 print(msg)
-
-
-
-
-
