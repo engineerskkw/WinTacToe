d2580ea5122fd98f693991827527d926fdf82c27
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-11-21 09:08:12 +0100

    Refactor Q learning agent and add performance measure

--- reinforcement_learning/agents/q_learning_agent/q_learning_agent.py
+++ reinforcement_learning/agents/q_learning_agent/q_learning_agent.py
@@ -1,7 +1,8 @@
 import sys, os
+import numpy as np
 from reinforcement_learning.abstract.abstract_agent import Agent
-from reinforcement_learning.agents.basic_mc_agent.action_value_derived_policy import ActionValueDerivedPolicy
-from reinforcement_learning.agents.basic_mc_agent.lazy_tabular_action_value import LazyTabularActionValue
+from reinforcement_learning.agents.common.action_value_derived_policy import ActionValueDerivedPolicy
+from reinforcement_learning.agents.common.lazy_tabular_action_value import LazyTabularActionValue
 
 # BEGIN--------------------PROJECT-ROOT-PATH-APPENDING-------------------------#
 REL_PROJECT_ROOT_PATH = "./../../../"
@@ -11,12 +12,12 @@ sys.path.append(ABS_PROJECT_ROOT_PATH)
 # -------------------------PROJECT-ROOT-PATH-APPENDING----------------------END#
 
 class QLearningAgent(Agent):
-    def __init__(self, step_size, epsilon, delta):
+    def __init__(self, step_size, epsilon, discount):
         self.action_value = LazyTabularActionValue()
         self.policy = ActionValueDerivedPolicy(self.action_value)
         self.step_size = step_size
         self.epsilon = epsilon
-        self.delta = delta
+        self.discount = discount
         self.performance_measure = []
 
         self._prev_action = None
@@ -41,17 +42,44 @@ class QLearningAgent(Agent):
     def exit(self, terminal_state):
         self._update(terminal_state)
         self.performance_measure.append(self._current_episode_return)
+        self.get_performance_graph(10)
         self._reset_prev_info()
 
     def _update(self, new_state):
         prev_action_value = self.action_value[self._prev_state, self._prev_action]
         error = self.step_size * \
-            (self._prev_reward + self.delta * self.action_value.max_over_actions(new_state) - prev_action_value)
+            (self._prev_reward + self.discount * self.action_value.max_over_actions(new_state) - prev_action_value)
         self.action_value[self._prev_state, self._prev_action] = prev_action_value + error
 
     def _reset_prev_info(self):
+        # print(self._current_episode_return)
         self._prev_action = None
         self._prev_state = None
         self._prev_reward = None
         self._current_episode_return = 0
+
+    def get_performance_graph(self, no_of_buckets):
+        no_of_episodes = len(self.performance_measure)
+        bucket_size = no_of_episodes // no_of_buckets
+        bins = np.array(range(0, no_of_episodes, bucket_size))
+
+        bin_numbers = np.digitize(self.performance_measure, bins)
+        buckets = [[] for _ in range(no_of_buckets)]
+
+        for bin_no, value in zip(bin_numbers, self.performance_measure):
+            buckets[bin_no - 1].append(value)
+
+        print(buckets)
+
+        ret = []
+
+        for bucket in buckets:
+            ret.append(np.average(bucket))
+
+        return ret
+
+
+
+
+
 
