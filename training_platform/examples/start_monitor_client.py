d846729206e457d67650b1f4e0b8825331c5f724
Jan <JanKapala@users.noreply.github.com>
2019-11-23 14:52:07 +0100

    Refactor comments in start_monitor

--- training_platform/examples/start_monitor_client.py
+++ training_platform/examples/start_monitor_client.py
@@ -11,7 +11,7 @@ from training_platform import MonitorClient
 from training_platform.common import LoggingLevel
 
 if __name__ == '__main__':
-    # Every log message has it logging level
+    # Every log message has it's logging level
     # There are few possible loging levels, e.g.:
     #   -> PLATFORM_COMMUNICATION_MESSAGES
     #   -> GAME_EVENTS
@@ -21,8 +21,9 @@ if __name__ == '__main__':
 
     # For example
     # logging_levels = list(LoggingLevel) # It's a list of all logging levels
-    # logging_levels = [
-    #     LoggingLevel.PLATFORM_COMMUNICATION_MESSAGES] # It's a list containing only one logging level GAME_EVENTS
+    # It's a list containing only one logging level PLATFORM_COMMUNICATION_MESSAGES
+    # logging_levels = [LoggingLevel.PLATFORM_COMMUNICATION_MESSAGES]
+    # And it's example of list usefull for debuging
     # logging_levels = [LoggingLevel.PLATFORM_COMMUNICATION_MESSAGES, LoggingLevel.DEBUG]
     logging_levels = [LoggingLevel.GAME_EVENTS]
     monitor = MonitorClient(logging_levels)
