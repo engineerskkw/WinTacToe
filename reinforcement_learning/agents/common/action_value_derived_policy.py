9b88b61fc5689a66eca84edf8ade85b24937d2db
Jan K. Kapala <jankapala@gmail.com>
2019-11-26 22:16:06 +0100

    Fix action value derrived policy representations

--- reinforcement_learning/agents/common/action_value_derived_policy.py
+++ reinforcement_learning/agents/common/action_value_derived_policy.py
@@ -1,6 +1,8 @@
 # BEGIN--------------------PROJECT-ROOT-PATH-APPENDING-------------------------#
 import sys, os
 
+from reinforcement_learning.agents.common.auxiliary_utilities import linear_map
+
 REL_PROJECT_ROOT_PATH = "./../../../"
 ABS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 ABS_PROJECT_ROOT_PATH = os.path.normpath(os.path.join(ABS_FILE_DIR, REL_PROJECT_ROOT_PATH))
@@ -8,7 +10,7 @@ sys.path.append(ABS_PROJECT_ROOT_PATH)
 # -------------------------PROJECT-ROOT-PATH-APPENDING----------------------END#
 
 import random
-
+from graphviz import Digraph
 from scipy.special import softmax
 from reinforcement_learning.base.base_policy import BasePolicy
 from reinforcement_learning.agents.common.lazy_tabular_action_value import LazyTabularActionValue
@@ -18,6 +20,9 @@ from all_tests.mock.test_mock_state import MockState
 
 
 class ActionValueDerivedPolicy(BasePolicy):
+    MIN_PEN_WIDTH = 1
+    MAX_PEN_WIDTH = 4
+
     def __init__(self, action_value):
         self.action_value = action_value
 
@@ -46,16 +51,46 @@ class ActionValueDerivedPolicy(BasePolicy):
         return action_space.random_action  # Otherwise (in each case) get a random action
 
     def __str__(self):
-        return self.action_value.__str__()
+        acc = ""
+
+        for state, actions in self.action_value.action_value_dict.items():
+            greedy_actions = self.action_value.argmax(state)
+            greedy_action_value = self.action_value.max(state)
+            acc += f"State: \n"
+            acc += f"{state}\n"
+            acc += f"Greedy value: {greedy_action_value}\n"
+            for action in greedy_actions:
+                acc += f"Action: {action}\n"
+
+        return acc
 
     def __repr__(self):
         return self.__str__()
 
+
+
+    def _get_graph(self):
+        graph = Digraph()
+        for state in self.action_value.action_value_dict.keys():
+            # Calculate sum of all actions' values from this state
+            greedy_actions = self.action_value.argmax(state)
+            greedy_action_value = self.action_value.max(state)
+
+            graph.node(str(hash(state)), str(state))
+            for greedy_action in greedy_actions:
+                graph.node(str(hash(greedy_action)), str(greedy_action))
+                # red = int(linear_map(value, 0, 255, actions.values()))
+                # color = '#%02x%02x%02x' % (red, 0, 0)
+                # penwidth = str(linear_map(value, self.MIN_PEN_WIDTH, self.MAX_PEN_WIDTH, actions.values()))
+                graph.edge(str(hash(state)), str(hash(greedy_action)), label=str(greedy_action_value),
+                           color="red", penwidth="2")
+        return graph
+
     def _repr_svg_(self):
-        return self.action_value._repr_svg_()
+        return self._get_graph()._repr_svg_()
 
     def view(self):
-        return self.action_value.view()
+        return self._get_graph().view()
 
 
 if __name__ == '__main__':
