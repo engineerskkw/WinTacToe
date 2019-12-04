480cac7f9105549b825faf286af5cf66d1f12f28
Jan K. Kapala <jankapala@gmail.com>
2019-12-04 17:21:24 +0100

    Adjust new LazyTabularActionValue to the new BaseActionValue

--- reinforcement_learning/agents_new/common/lazy_tabular_action_value.py
+++ reinforcement_learning/agents_new/common/lazy_tabular_action_value.py
@@ -39,15 +39,15 @@ class LazyTabularActionValue(BaseActionValue):
         new_estimate = old_estimate + step_size * (target - old_estimate)
         self.action_value_dict[state][action] = new_estimate
 
-    def max(self, state):
+    def max(self, state, action_space=None):
         expected_returns = self.action_value_dict[state].values()
         return max(expected_returns) if expected_returns else self._default_cell_value
 
-    def argmax(self, state):
+    def argmax(self, state, action_space=None):
         actions = self.action_value_dict[state]
         return {key for (key, value) in actions.items() if value == max(actions.values())}
 
-    def action_returns(self, state):
+    def action_returns(self, state, action_space=None):
         return self.action_value_dict[state]
 
     @property
