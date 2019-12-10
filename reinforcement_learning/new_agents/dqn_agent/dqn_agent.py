1979d4e7a59b8eeae8ceeb111938c2be9f0caaf4
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2019-12-10 13:15:33 +0100

    Updated WORKING DQN agent

--- reinforcement_learning/new_agents/dqn_agent/dqn_agent.py
+++ reinforcement_learning/new_agents/dqn_agent/dqn_agent.py
@@ -17,16 +17,17 @@ import tensorflow as tf
 import numpy as np
 from bidict import bidict
 from environments.tic_tac_toe.tic_tac_toe_engine_utils import TicTacToeAction
+from environments.tic_tac_toe.tic_tac_toe_engine_utils import TicTacToeActionSpace
+import random
 
 @dataclass
 class MemoryElement:
-    state_0: BaseState
+    state: BaseState
     action: BaseAction
-    reward: float
-    state_1: BaseState
+    update_target: float
 
 class DQNAgent(BaseAgent):
-    def __init__(self, step_size, epsilon, discount):
+    def __init__(self, step_size, epsilon, discount, fit_period, batch_size):
         super().__init__()
         self.step_size = step_size
         self.epsilon = epsilon
@@ -43,52 +44,106 @@ class DQNAgent(BaseAgent):
                            metric=['accuracy'])
 
         self.n = 3
-        self.action_index = bidict({self.n*i+j:TicTacToeAction(i, j) for i in range(self.n) for j in range(self.n)})
+        self.action_index_dict = bidict({self.n * i + j:TicTacToeAction(i, j) for i in range(self.n) for j in range(self.n)})
 
-    def _max(self, state):
-        return max(self.model(self.get_features(state)))
+        self.prev_state = None
+        self.prev_action = None
+        self.tmp_reward = None
 
-    def _argmax(self, state):
-        return np.argmax()
+        self.iter = 0
+        self.fit_period = fit_period
+        self.batch_size = batch_size
+
 
     def take_action(self, state, allowed_actions):
-        if self._prev_state:
-            self._update(state, allowed_actions)
+        self.iter += 1
 
-        action = self.policy.epsilon_greedy(state, allowed_actions, self.epsilon)
-        self._prev_action = action
-        self._prev_state = state
+        if self.prev_state:
+            update_target = self.tmp_reward + self.discount * self.__max(state, allowed_actions)
+            self.__store(self.prev_state, self.prev_action, update_target)
+
+        action = self.__epsilon_greedy(state, allowed_actions)
+
+        self.prev_state = state
+        self.prev_action = action
 
         return action
 
     def receive_reward(self, reward):
-        # print(reward)
-        self._prev_reward = reward
+        self.tmp_reward = reward
         self._current_episode_return += reward
 
     def exit(self, terminal_state):
-        self._update(terminal_state, None)
+        self.iter += 1
+
+        update_target = self.tmp_reward
+        self.__store(self.prev_state, self.prev_action, update_target)
+
         self.all_episodes_returns.append(self._current_episode_return)
         self._reset_episode_info()
 
     def restart(self):
         self._reset_episode_info()
 
-    def _update(self, new_state, allowed_actions):
-        update_target = self._prev_reward + self.discount * self.action_value.max(new_state, allowed_actions)
+    def __max(self, state, action_space):
+        if action_space is None:
+            return 0.
 
-        self.action_value.sample_update(state=self._prev_state,
-                                        action=self._prev_action,
-                                        step_size=self.step_size,
-                                        target=update_target)
+        features = self.__get_features(state)
+        all_action_values = self.model(features)
+
+        available_action_indices = [self.action_index_dict.inverse[action] for action in action_space.actions]
+        return np.max(all_action_values[available_action_indices])
+
+    def __argmax(self, state, action_space):
+        features = self.__get_features(state)
+        all_action_values = self.model(features)
+
+        available_action_indices = [self.action_index_dict.inverse[action] for action in action_space.actions]
+
+        max_action_value = self.__max(state, action_space)
+        return {self.action_index_dict[available_index] for available_index in available_action_indices
+                if np.isclose(all_action_values[available_index], max_action_value)}
+
+    def __get_features(self, state):
+        not_normalized_features = state.flatten()
+        return not_normalized_features / np.linalg.norm(not_normalized_features)
+
+    def __epsilon_greedy(self, state, action_space):
+        if random.random() > self.epsilon:  # Choose action in the epsilon-greedy way
+            greedy_actions = self.__argmax(state, action_space)
+
+            if greedy_actions:  # Check if there are any chosen possibilities
+                return random.choice(list(greedy_actions))
+
+        return action_space.random_action  # Otherwise (in each case) get a random action
+
+    def __store(self, state, action, update_target):
+        self.memory.append(MemoryElement(state, action, update_target))
+
+        if self.iter % self.fit_period == 0 and len(self.memory) >= self.batch_size:
+            self.__fit_batch()
+
+    def __fit_batch(self):
+        chosen_elements = random.sample(self.memory, self.batch_size)
+
+        batch_X = np.array([self.__get_features(element.state) for element in chosen_elements])
+        predictions = self.model(batch_X)
+
+        for i in range(len(chosen_elements)):
+            action_index = self.action_index_dict.inverse(chosen_elements[i].action)
+            predictions[i][action_index] = chosen_elements[i].update_target
+
+        batch_Y = predictions
+
+        self.model.fit(batch_X, batch_Y, verbose=0, epochs=1)
 
     def _reset_episode_info(self):
-        self._prev_action = None
-        self._prev_state = None
-        self._prev_reward = None
         self._current_episode_return = 0
+        self.prev_state = None
+        self.prev_action = None
+        self.tmp_reward = None
 
 
 if __name__ == "__main__":
-    dqn = DQNAgent(0.3, 0.1, 0.1)
-    print(dqn.action_index)
+    print('a')
