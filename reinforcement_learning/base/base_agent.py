156a7b727ff3d34427d806ff7f45b92c666e2bfa
Jan Kapała <jankapala@gmail.com>
2019-12-11 20:14:12 +0100

    Add bucketing as an option in visualisation

--- reinforcement_learning/base/base_agent.py
+++ reinforcement_learning/base/base_agent.py
@@ -16,6 +16,7 @@ import matplotlib.gridspec as gridspec
 
 from reinforcement_learning.agents.common.agent_utils import bucketify
 from utils.common_utils import return_deepcopy
+import matplotlib.ticker as mtick
 
 class BaseAgent(ABC):
     def __init__(self):
@@ -88,39 +89,49 @@ class BaseAgent(ABC):
         # TODO seamlessly integrate matplotlib plot with ipython _repr_svg_
         self.visualize()
 
-    def visualize(self):
-        er_n = len(self.all_episodes_returns)
-        er_buckets_number = min(int(np.ceil(0.1*er_n)), 100)
-        er_bucket_size = int(er_n/er_buckets_number)
-        performance = bucketify(self.all_episodes_returns, er_buckets_number, np.mean)
+    def visualize(self, bucketing=True):
+        if bucketing:
+            er_n = len(self.all_episodes_returns)
+            er_buckets_number = min(int(np.ceil(0.1*er_n)), 100)
+            er_bucket_size = int(er_n/er_buckets_number)
+            performance = (np.array(bucketify(self.all_episodes_returns, er_buckets_number, np.mean))+1)/2.0
+        else:
+            performance = self.all_episodes_returns
         mean_reward = np.mean(self.all_episodes_returns)
 
         ep_act_times_flatten = [item for sublist in self.episodes_actions_times for item in sublist]
         mean_action_time = np.mean(ep_act_times_flatten)
-        at_n = len(ep_act_times_flatten)
-        at_buckets_number = min(int(np.ceil(0.1*er_n)), 100)
-        at_bucket_size = int(at_n / at_buckets_number)
-        action_times = bucketify(ep_act_times_flatten, at_buckets_number, np.mean)
+        if bucketing:
+            at_n = len(ep_act_times_flatten)
+            at_buckets_number = min(int(np.ceil(0.1*er_n)), 100)
+            at_bucket_size = int(at_n / at_buckets_number)
+            action_times = bucketify(ep_act_times_flatten, at_buckets_number, np.mean)
+        else:
+            action_times = ep_act_times_flatten
 
         fig = plt.figure(constrained_layout=True)
         fig.suptitle(f"{self.__class__.__name__}", fontsize=16, y=1.1)
         spec = gridspec.GridSpec(ncols=1, nrows=2, figure=fig)
 
         ax0 = fig.add_subplot(spec[0, 0])
-        ax0.set_xlabel(f"Episodes buckets ({er_buckets_number} buckets of size {er_bucket_size})")
-        ax0.set_ylabel("Mean return")
+        if bucketing:
+            ax0.set_xlabel(f"Episodes buckets ({er_buckets_number} buckets of size {er_bucket_size})")
+            ax0.set_ylabel("Percentage of winnings")
+        else:
+            ax0.set_ylabel("Mean return")
         ax0.set_title(f"Performance")
         ax0.plot(performance)
 
         ax1 = fig.add_subplot(spec[1, 0])
-        ax1.set_xlabel(f"Actions buckets ({at_buckets_number} buckets of size {at_bucket_size})")
+        if bucketing:
+            ax1.set_xlabel(f"Actions buckets ({at_buckets_number} buckets of size {at_bucket_size})")
         ax1.set_ylabel("Mean action time [s]")
         ax1.set_title(f"Action times")
         ax1.plot(action_times)
 
         plt.show()
 
-        print(f"Number of played episodes: {er_n}")
+        # print(f"Number of played episodes: {er_n}")
         print(f"Mean reward: {'{0:0.6f}'.format(mean_reward)}")
-        print(f"Number of taken actions: {at_n}")
+        # print(f"Number of taken actions: {at_n}")
         print(f"Mean action time: {'{0:0.6f}'.format(mean_action_time)}")
