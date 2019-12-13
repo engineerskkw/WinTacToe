758c26fc5c9758bc2a5a6d0257fd4eb10a3ac2bd
Jan Kapała <jankapala@gmail.com>
2019-12-13 21:19:59 +0100

    Add utillity needed for agents with keras model saving

--- reinforcement_learning/agents/common/agent_utils.py
+++ reinforcement_learning/agents/common/agent_utils.py
@@ -7,6 +7,8 @@ ABS_PROJECT_ROOT_PATH = os.path.normpath(os.path.join(ABS_FILE_DIR, REL_PROJECT_
 sys.path.append(ABS_PROJECT_ROOT_PATH)
 # -------------------------PROJECT-ROOT-PATH-APPENDING----------------------END#
 
+import time
+import random
 
 def bucketify(data, no_of_buckets, reduce_function):
     bucket_size = len(data) // no_of_buckets
@@ -24,3 +26,7 @@ def bucketify(data, no_of_buckets, reduce_function):
 
 def safe_return(returns, index):
     return returns[index] if index < len(returns) else 0
+
+def gen_tempfile_path(agent_path):
+    unique_number = str(int(float(time.time())*10**7)) + str(random.randint(10**10, 2*10**10))
+    return os.path.join(os.path.dirname(agent_path), f"tmp_{unique_number}.h5")
