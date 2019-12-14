28d0bf5c718fddbfa00058e2e0fe63051342a16d
Jan Kapała <jankapala@gmail.com>
2019-12-14 02:14:49 +0100

    Fix test

--- all_tests/reinforcement_learning/test_agents_saving_and_loading.py
+++ all_tests/reinforcement_learning/test_agents_saving_and_loading.py
@@ -1,5 +1,6 @@
 # BEGIN--------------------PROJECT-ROOT-PATH-APPENDING-------------------------#
 import sys, os
+
 REL_PROJECT_ROOT_PATH = "./../../"
 ABS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 ABS_PROJECT_ROOT_PATH = os.path.normpath(os.path.join(ABS_FILE_DIR, REL_PROJECT_ROOT_PATH))
@@ -14,6 +15,7 @@ from reinforcement_learning.common.simple_training import SimpleTraining
 from environments.tic_tac_toe.tic_tac_toe_engine import TicTacToeEngine
 from reinforcement_learning.new_agents.n_step_agent.n_step_agent import NStepAgent
 from reinforcement_learning.base.base_agent import BaseAgent
+from reinforcement_learning.new_agents.common.epsilon_strategy import CircleEpsilonStrategy
 
 
 class TestAgentsSavingAndLoading(TestCase):
@@ -23,24 +25,18 @@ class TestAgentsSavingAndLoading(TestCase):
         self.agent_path = os.path.join(ABS_PROJECT_ROOT_PATH, "all_tests", "reinforcement_learning", "agent.ai")
 
     def test_saving_and_loading_for_agent_with_model(self):
-        hyper_epsilon_starting_value = 0.3
-        x = np.linspace(0, 10 * np.pi, int(0.9 * self.number_of_episodes))
-        y = np.abs((np.sin(x) / x)) * hyper_epsilon_starting_value
-        example_epsilon_iterator0 = iter(y)
-        example_epsilon_iterator1 = iter(y)
-
         agents = [DQNAgent(step_size=0.01,
-                           epsilon_iter=example_epsilon_iterator0,
                            discount=1,
-                           fit_period=4,
-                           batch_size=4,
-                           max_memory_size=4),
+                           epsilon_strategy=CircleEpsilonStrategy(0.1, 0.7),
+                           fit_period=64,
+                           batch_size=64,
+                           max_memory_size=64),
                   DQNAgent(step_size=0.01,
-                           epsilon_iter=example_epsilon_iterator1,
                            discount=1,
-                           fit_period=4,
-                           batch_size=4,
-                           max_memory_size=4)
+                           epsilon_strategy=CircleEpsilonStrategy(0.1, 0.7),
+                           fit_period=64,
+                           batch_size=64,
+                           max_memory_size=64)
                   ]
 
         with SimpleTraining(self.engine, agents) as st:
@@ -57,8 +53,8 @@ class TestAgentsSavingAndLoading(TestCase):
             self.assertTrue(np.all(np.isclose(original_weights[i], loaded_weights[i])))
 
     def test_saving_and_loading_for_agent_without_model(self):
-        agents = [NStepAgent(5, 0.1, 0.2, 1),
-                  NStepAgent(5, 0.1, 0.2, 1)
+        agents = [NStepAgent(5, 0.1, CircleEpsilonStrategy(0.1, 0.7), 1),
+                  NStepAgent(5, 0.1, CircleEpsilonStrategy(0.1, 0.7), 1)
                   ]
 
         with SimpleTraining(self.engine, agents) as st:
@@ -68,3 +64,6 @@ class TestAgentsSavingAndLoading(TestCase):
         agent.save(self.agent_path)
         loaded_agent = BaseAgent.load(self.agent_path)
         self.assertTrue(np.all(np.isclose(loaded_agent.all_episodes_returns, agent.all_episodes_returns)))
+
+    def tearDown(self):
+        os.remove(self.agent_path)
