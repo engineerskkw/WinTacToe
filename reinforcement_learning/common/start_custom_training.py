62ff6da8d00d1edcd85dabdcfede70ef93ada4a1
Jan Kapała <jankapala@gmail.com>
2019-12-17 13:06:15 +0100

    Modify custom training

--- reinforcement_learning/common/start_custom_training.py
+++ reinforcement_learning/common/start_custom_training.py
@@ -16,6 +16,7 @@ from reinforcement_learning.base.base_agent import BaseAgent
 from reinforcement_learning.new_agents.n_step_agent.n_step_agent import NStepAgent
 from reinforcement_learning.new_agents.random_agent.random_agent import RandomAgent
 from reinforcement_learning.new_agents.dqn_agent.dqn_agent import DQNAgent
+from reinforcement_learning.agents.basic_mc_agent.basic_mc_agent import BasicAgent
 
 # Agents building blocks
 from reinforcement_learning.new_agents.common.epsilon_strategy import ConstantEpsilonStrategy, CircleEpsilonStrategy, DecayingSinusEpsilonStrategy
@@ -43,19 +44,25 @@ if __name__ == '__main__':
     #                    batch_size=64,
     #                    max_memory_size=64)]
 
-    agents = [NStepAgent(n=5,
-                         step_size=0.1,
-                         epsilon_strategy=CircleEpsilonStrategy(starting_epsilon_value=0.1, exploration_part=0.7),
+    # agents = [NStepAgent(n=5,
+    #                      step_size=0.1,
+    #                      epsilon_strategy=CircleEpsilonStrategy(starting_epsilon_value=0.1, exploration_part=0.7),
+    #                      discount=1),
+    #           NStepAgent(n=5,
+    #                      step_size=0.1,
+    #                      epsilon_strategy=CircleEpsilonStrategy(starting_epsilon_value=0.1, exploration_part=0.7),
+    #                      discount=1)]
+
+    agents = [BasicAgent(epsilon_strategy=ConstantEpsilonStrategy(0.1),
                          discount=1),
-              NStepAgent(n=5,
-                         step_size=0.1,
-                         epsilon_strategy=CircleEpsilonStrategy(starting_epsilon_value=0.1, exploration_part=0.7),
-                         discount=1),]
+              BasicAgent(epsilon_strategy=ConstantEpsilonStrategy(0.1),
+                         discount=1)]
 
     episodes_no = 10000
     with SimpleTraining(engine=engine, agents=agents) as st:
         agents = st.train(episodes_no=episodes_no,
                           auto_saving=True,
-                          saving_description="NStepAgent vs NStepAgent from scratch")
+                          saving_description="MonteCarloAgent vs MonteCarloAgent from scratch, "
+                                             "sConstantEpsilonStrategy(0.1), discount=1")
 
     [agent.visualize() for agent in agents]
