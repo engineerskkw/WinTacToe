5dc7e36c6a6bf6ae02c4939621adf36050d0447a
jan <jankapala@gmail.com>
2020-05-10 20:57:43 +0200

    Update start agent client scripts to fit the new agent constructor

--- training_platform/examples/distributed_initialization/start_agent_client_1.py
+++ training_platform/examples/distributed_initialization/start_agent_client_1.py
@@ -1,10 +1,11 @@
 from training_platform.server.environment_server import EnvironmentServer
 from training_platform import AgentClient
 from reinforcement_learning.agents.monte_carlo_agent.monte_carlo_agent import MonteCarloAgent
+from reinforcement_learning.agents.common_building_blocks.epsilon_strategy import CircleEpsilonStrategy
 
 if __name__ == '__main__':
     server = EnvironmentServer()
     p = server.players[1]
-    c = AgentClient(MonteCarloAgent())
+    c = AgentClient(MonteCarloAgent(CircleEpsilonStrategy(starting_epsilon_value=0.1, exploration_part=0.7)))
     c.join(p)
     print("BaseAgent Client has joind server!")
